9278fa7e8f79fd1647cd98bf65e6416c7123ae9c
neumanncc <145790372+equqe@users.noreply.github.com>
2024-05-04 00:28:16 +0300

    better use enum and data if db supports it

--- src/resume/models.py
+++ src/resume/models.py
@@ -1,6 +1,11 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Text, Enum
 
 metadata = MetaData()
+
+class Sex(Enum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
 
 resume = Table(
     "resume",
@@ -8,10 +13,10 @@ resume = Table(
     Column("id", Integer, nullable=False, primary_key=True),
     Column("job_title", String, nullable=False),
     Column("age", Integer, nullable=False),
-    Column("sex", String, nullable=False),
-    Column("birth_date", String, nullable=False),
+    Column("sex", Enum(Sex), nullable=False),
+    Column("birth_date", Date, nullable=False),
     Column("lived_in", String, nullable=False),
     Column("want_salary", Integer),
     Column("status", String),
-    Column("about", String),
+    Column("about", Text),
 )
