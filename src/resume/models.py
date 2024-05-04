8b65d804b5f4b3573f3fb9a024a67c4cdc4c16f3
Shin Kranel <shinkranel@gmail.com>
2024-05-04 14:18:19 +0100

    Update models

--- src/resume/models.py
+++ src/resume/models.py
@@ -2,10 +2,20 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Text, Enu
 
 metadata = MetaData()
 
+
 class Sex(Enum):
     male = 'male'
     female = 'female'
     other = 'other'
+
+
+class InterestStatus(Enum):
+    """Candidate's job interest status"""
+    looking_for_job = 'looking for job'
+    not_looking_for_a_job = 'not looking for a job'
+    considers_proposals = 'considers proposals'
+    offered_a_job_decides = 'offered a job, decides'
+
 
 resume = Table(
     "resume",
@@ -17,6 +27,6 @@ resume = Table(
     Column("birth_date", Date, nullable=False),
     Column("lived_in", String, nullable=False),
     Column("want_salary", Integer),
-    Column("status", String),
+    Column("status", Enum(InterestStatus)),
     Column("about", Text),
 )
--- src/vacancy/models.py
+++ src/vacancy/models.py
@@ -1,6 +1,21 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Table, Column, Integer, String, MetaData, Enum, Text
 
 metadata = MetaData()
+
+
+class WorkFormat(Enum):
+    office = 'in office'
+    home = 'from home'
+    hybrid = 'hybrid'
+
+
+class WorkExperience(Enum):
+    no_experience = 'no experience'
+    up_to_1_year = 'up to 1 year'
+    between_1_and_3 = '1-3 years'
+    between_3_and_6 = '3-6 years'
+    more_than_6 = 'more than 6 years'
+
 
 vacancy = Table(
     "vacancy",
@@ -8,9 +23,9 @@ vacancy = Table(
     Column("id", Integer, nullable=False, primary_key=True),
     Column("job_title", String, nullable=False),
     Column("company", String, nullable=False),
-    Column("work_experience", Integer, nullable=False),
-    Column("work_format", String, nullable=False),
+    Column("work_experience", Enum(WorkExperience), nullable=False),
+    Column("work_format", Enum(WorkFormat), nullable=False),
     Column("salary", Integer),
-    Column("skills", Integer),
-    Column("about", Integer, nullable=False),
+    Column("skills", String),  # TODO: change to list-like
+    Column("about", Text, nullable=False),
 )
