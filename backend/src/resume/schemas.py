2f4f954afaf1195ec26b6afad03ca7f5de5543c9
Shin Kranel <shinkranel@gmail.com>
2024-06-05 15:26:40 +0100

    Add read and update schemas for resume

--- backend/src/resume/schemas.py
+++ backend/src/resume/schemas.py
@@ -23,3 +23,37 @@ class CreateResume(BaseModel):
 
     user_id: int = 1
     vacancy_id: int = 1
+
+
+class ReadResume(BaseModel):
+    first_name: str
+    last_name: str
+    job_title: str
+    age: int
+    gender: Gender
+    city: str
+    expected_salary: int
+    resume_status: ResumeStatus
+    skills: str
+    about: str
+    experience: str
+    education: str
+    ready_to_relocate: bool
+    ready_for_business_trips: bool
+
+
+class UpdateResume(BaseModel):
+    first_name: str | None
+    last_name: str | None
+    job_title: str | None
+    age: int | None
+    gender: Gender | None
+    city: str | None
+    expected_salary: int | None
+    resume_status: ResumeStatus | None
+    skills: str | None
+    about: str | None
+    experience: str | None
+    education: str | None
+    ready_to_relocate: bool | None
+    ready_for_business_trips: bool | None
