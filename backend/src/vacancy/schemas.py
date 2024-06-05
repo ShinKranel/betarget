9cf126a6f9a4561b60f25e30e9cde74f27d52c21
Shin Kranel <shinkranel@gmail.com>
2024-06-05 15:18:34 +0100

    Add read and update schemas for vacancy

--- backend/src/vacancy/schemas.py
+++ backend/src/vacancy/schemas.py
@@ -16,3 +16,29 @@ class CreateVacancy(BaseModel):
     description: str
 
     user_id: int = 1
+
+
+class ReadVacancy(BaseModel):
+    job_title: str
+    city: str
+    company: str
+    experience: Experience
+    work_format: WorkFormat
+    salary: int
+    education: Education
+    employment_type: EmploymentType
+    skills: str
+    description: str
+
+
+class UpdateVacancy(BaseModel):
+    job_title: str | None
+    city: str | None
+    company: str | None
+    experience: Experience | None
+    work_format: WorkFormat | None
+    salary: int | None
+    education: Education | None
+    employment_type: EmploymentType | None
+    skills: str | None
+    description: str | None
