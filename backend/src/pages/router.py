ce7dede84c25d752b1dacc1dced4be75ef1cdc34
Shin Kranel <shinkranel@gmail.com>
2024-06-08 15:07:47 +0100

    Nothing much

--- backend/src/pages/router.py
+++ backend/src/pages/router.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+import mimetypes
+mimetypes.init()
+
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="frontend/src/templates")
 
 
-@router.get("/crm")
+@router.get("/crm", response_class=HTMLResponse)
 def get_base_template(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    mimetypes.add_type('application/javascript', '.js')
+    mimetypes.add_type('text/css', '.css')
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse("index.html", context)
