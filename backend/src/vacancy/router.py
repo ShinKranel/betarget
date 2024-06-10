f0dfd85e9208ff6e0e8d4ae9cc62828407fb5de1
Shin Kranel <shinkranel@gmail.com>
2024-06-10 16:27:14 +0100

    linked the receipt of vacancies with the user

--- backend/src/vacancy/router.py
+++ backend/src/vacancy/router.py
@@ -1,43 +1,67 @@
-import asyncio
-
 from fastapi import APIRouter, Depends
 from sqlalchemy import insert, select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
+from backend.src.auth.models import User
 from backend.src.db import get_async_session
 from backend.src.vacancy.models import Vacancy
-from backend.src.vacancy.schemas import VacancyCreate, VacanciesRead
+from backend.src.vacancy.schemas import VacancyCreate
+
+from backend.src.auth.base_config import current_user
 
 router = APIRouter()
 
 
-@router.get("/")
-async def get_vacancies(session: AsyncSession = Depends(get_async_session)):
-    query = select(Vacancy)
+@router.get(
+    "/",
+    name="vacancy:read_user_vacancies"
+)
+async def read_user_vacancies(
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
+):
+    query = select(Vacancy).where(user.id == Vacancy.user_id)
     result = await session.execute(query)
     vacancies = result.scalars().all()
     return vacancies
 
 
-@router.get("/{vacancy_id}")
-async def get_vacancy_by_id(vacancy_id: int, session: AsyncSession = Depends(get_async_session)):
-    query = select(Vacancy).where(Vacancy.id == vacancy_id)
+@router.get(
+    "/{vacancy_id}",
+    name="vacancy:read_user_vacancy"
+)
+async def read_user_vacancy(vacancy_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Vacancy).where(vacancy_id == Vacancy.id)
     result = await session.execute(query)
     vacancies = result.scalars().all()
     return vacancies
 
 
-@router.post("/")
-async def add_vacancy(new_vacancy: VacancyCreate, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(Vacancy).values(**new_vacancy.dict())
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    name="vacancy:create-vacancy"
+)
+async def create_vacancy(
+        new_vacancy: VacancyCreate,
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
+):
+    new_vacancy = new_vacancy.dict()
+    new_vacancy.update({"user_id": user.id})
+    stmt = insert(Vacancy).values(**new_vacancy)
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
 
 
-@router.delete("/")
+@router.delete(
+    "/",
+    name="vacancy:delete_vacancy"
+)
 async def delete_vacancy(vacancy_id: int, session: AsyncSession = Depends(get_async_session)):
-    stmt = delete(Vacancy).where(Vacancy.id == vacancy_id)
+    stmt = delete(Vacancy).where(vacancy_id == Vacancy.id)
     await session.execute(stmt)
     await session.commit()
     return {"status": "vacancy was deleted"}
