9db832dcbff6d0882d47b919603b45a082d269ab
Shin Kranel <shinkranel@gmail.com>
2024-06-10 16:30:58 +0100

    moved fastapi_user and current_user to base_config.py

--- backend/src/auth/base_config.py
+++ backend/src/auth/base_config.py
@@ -1,6 +1,9 @@
+from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import CookieTransport, AuthenticationBackend
 from fastapi_users.authentication import JWTStrategy
 
+from backend.src.auth.manager import get_user_manager
+from backend.src.auth.models import User
 from backend.src.config import SECRET_JWT
 
 cookie_transport = CookieTransport(
@@ -20,3 +23,10 @@ auth_backend = AuthenticationBackend(
     transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_user = fastapi_users.current_user()
