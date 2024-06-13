c024af7fb97d377e72e02a33cd63e5df97911e70
Shin Kranel <shinkranel@gmail.com>
2024-06-13 19:51:58 +0100

    add test db data (update later)

--- backend/src/config.py
+++ backend/src/config.py
@@ -10,6 +10,12 @@ DB_PASS = os.environ.get("DB_PASS")
 DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 
+DB_TEST_USER = os.environ.get("DB_TEST_USER")
+DB_TEST_PORT = os.environ.get("DB_TEST_PORT")
+DB_TEST_PASS = os.environ.get("DB_TEST_PASS")
+DB_TEST_HOST = os.environ.get("DB_TEST_HOST")
+DB_TEST_NAME = os.environ.get("DB_TEST_NAME")
+
 BACKEND_CORS_ORIGINS = os.environ.get("BACKEND_CORS_ORIGINS")
 
 
