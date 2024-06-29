e1aebae37f5a97a3d0a9db546b50a54fc5ddccf0
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-06-29 17:51:56 +0300

    add resume change status

--- frontend/src/static/js/actions/eventListeners.js
+++ frontend/src/static/js/actions/eventListeners.js
@@ -1,5 +1,10 @@
 import { setActiveVacancy } from "./vacancyActions.js";
 import { setActiveResume } from "./resumeListActions.js";
+import {
+  handleChangeStageButton,
+  handleStageInputChanges,
+  handleSaveStageButton,
+} from "./resumeActions.js";
 
 function addEventListenerToVacancy(vacancyElement) {
   vacancyElement.addEventListener("click", setActiveVacancy);
@@ -9,4 +14,22 @@ function addEventListenerToResumeList(resumeElement) {
   resumeElement.addEventListener("click", setActiveResume);
 }
 
-export { addEventListenerToVacancy, addEventListenerToResumeList };
+function addEventListenerToResumeChangeStageButton(buttonElement) {
+  buttonElement.addEventListener("click", handleChangeStageButton);
+}
+
+function addEventListenerToResumeStageInput(inputElement) {
+  inputElement.addEventListener("change", handleStageInputChanges);
+}
+
+function addEventListenerToResumeSaveStageButton(saveStageButton) {
+  saveStageButton.addEventListener("change", handleSaveStageButton);
+}
+
+export {
+  addEventListenerToVacancy,
+  addEventListenerToResumeList,
+  addEventListenerToResumeChangeStageButton,
+  addEventListenerToResumeStageInput,
+  addEventListenerToResumeSaveStageButton,
+};
