2c89ef1d5e7b53ac9ceac191116612eda0976c61
AurunChill <aurunchill@yandex.ru>
2024-07-04 13:55:03 +0300

    Adding specific validation for schemas (#180)

--- backend/src/vacancy/schemas.py
+++ backend/src/vacancy/schemas.py
@@ -1,35 +1,34 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from backend.src.vacancy.models import WorkFormat, Experience, Education, EmploymentType
 
 
-# todo: add restrictions to every param
 class VacancyCreate(BaseModel):
-    job_title: str
-    city: str | None
-    company: str
+    job_title: str = Field(..., min_length=1, max_length=60)
+    city: str | None = Field(None, max_length=50)
+    company: str = Field(..., min_length=1, max_length=50)
     experience: Experience | None
     work_format: WorkFormat | None
-    salary: int | None
+    salary: int | None = Field(None, ge=0)
     education: Education | None
     employment_type: EmploymentType | None
-    skills: list[str] | None
-    description: str | None
+    skills: list[str] | None = Field(None, max_items=20)
+    description: str | None = Field(None, max_length=2000)
 
 
 class VacancyRead(BaseModel):
     id: int
     user_id: int
-    job_title: str | None
-    city: str | None
-    company: str
+    job_title: str | None = Field(None, min_length=1, max_length=60)
+    city: str | None = Field(None, max_length=50)
+    company: str = Field(..., min_length=1, max_length=50)
     experience: Experience | None
     work_format: WorkFormat | None
-    salary: int | None
+    salary: int | None = Field(None, ge=0)
     education: Education | None
     employment_type: EmploymentType | None
-    skills: list[str] | None
-    description: str | None
+    skills: list[str] | None = Field(None, max_items=20)
+    description: str | None = Field(None, max_length=2000)
 
 
 class VacancyUpdate(VacancyRead):
