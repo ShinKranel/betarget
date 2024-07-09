9e0eec260a8a2ae52ba069095d0641efea956ade
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-09 08:10:37 +0300

    add rating slider

--- frontend/src/static/js/render.js
+++ frontend/src/static/js/render.js
@@ -192,6 +192,33 @@ function renderResume(resume) {
   aboutParagraph.textContent = resume.about;
 
   // Resume info
+  const slider = document.getElementById("resume-info__slider");
+  const rating = document.getElementById("resume-info__rating");
+  const decreaseButton = document.getElementById("resume-info__decrease");
+  const increaseButton = document.getElementById("resume-info__increase");
+
+  slider.addEventListener("input", () => {
+    const value = slider.value;
+    const step = 1; // шаг деления
+    const snappedValue = Math.round(value / step) * step;
+    slider.value = snappedValue;
+    rating.textContent = snappedValue;
+  });
+
+  decreaseButton.addEventListener("click", () => {
+    const currentValue = parseInt(slider.value);
+    const newValue = Math.max(0, currentValue - 1);
+    slider.value = newValue;
+    rating.textContent = newValue;
+  });
+
+  increaseButton.addEventListener("click", () => {
+    const currentValue = parseInt(slider.value);
+    const newValue = Math.min(10, currentValue + 1);
+    slider.value = newValue;
+    rating.textContent = newValue;
+  });
+
   const resumeInfoName = resumeInfoSection.querySelector(".resume-info__name");
   resumeInfoName.textContent = `${resume.firstName} ${resume.lastName}`;
 
